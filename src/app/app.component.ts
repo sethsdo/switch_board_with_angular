ddac55275aa74a078c7b0845d8fe9c3e97934073
sethsdo <setholmstead@gmail.com>
2017-10-11 14:18:55 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  color1 = "red";
+  color2 = "green";
+
+  show1: boolean = true;
+  show2: boolean = true;
+  show3: boolean = true;
+  show4: boolean = true;
+  show5: boolean = true;
+  show6: boolean = true;
+  show7: boolean = true;
+  show8: boolean = true;
+  show9: boolean = true;
+  show10: boolean = true;
+  
+}
